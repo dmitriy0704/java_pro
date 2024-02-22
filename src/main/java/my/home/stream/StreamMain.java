8d254f82b7483adcd92a198218c4323a4684c60a
Dmitriy Folomkin <dmitriy.folomkin@gmail.com>
2024-02-22 21:53:22 +0300

    create collections...

--- src/main/java/my/home/stream/StreamMain.java
+++ src/main/java/my/home/stream/StreamMain.java
@@ -1,7 +1,43 @@
 package my.home.stream;
 
-public class StreamMain {
-    public static void main(String[] args) {
 
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class StreamMain {
+
+    private static class Person {
+        private String name;
+        private String position;
+        private int age;
+
+        public Person(String name, String position, int age) {
+            this.name = name;
+            this.position = position;
+            this.age = age;
+        }
+    }
+
+    public static void main(String[] args) {
+        List<Person> persons = new ArrayList<>(
+                Arrays.asList(
+                        new Person("Bob1", "Engineer", 45),
+                        new Person("Bob2", "Engineer", 37),
+                        new Person("Bob3", "Manager", 31),
+                        new Person("Bob4", "Manager", 34),
+                        new Person("Bob5", "Engineer", 44),
+                        new Person("Bob6", "CEO", 50)
+                )
+        );
+
+        List<Person> engineers = new ArrayList<>();
+        for (Person p : persons) {
+            if (p.position.equals("Engineer")) {
+                engineers.add(p);
+            }
+        }
     }
 }
